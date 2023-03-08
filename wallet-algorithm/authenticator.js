9e067841411652fbfc6d4d8aaaae7a3c4d22b910
yathin017 <yathinprakash017@gmail.com>
2023-03-08 21:34:05 +0530

    Backend

--- wallet-algorithm/authenticator.js
+++ wallet-algorithm/authenticator.js
@@ -1,16 +1,16 @@
 const speakeasy = require('speakeasy')
 
 // Generate key
-var key = speakeasy.generateSecret({
-    name: "wallet.eth"
-})
-console.log(key);
+// var key = speakeasy.generateSecret({
+//     name: "wallet.eth"
+// })
+// console.log(key);
 
 // Verify
 const verify = speakeasy.totp.verify({
-    secret: '',
+    secret: 'KRQW6XSHGZTVEMSXPV4HAQ2WNM7VGPCNJE2VUKBIENFW442RPI4Q',
     encoding: 'ascii',
-    token: ''
+    token: '608253'
 })
 
 console.log(verify);
